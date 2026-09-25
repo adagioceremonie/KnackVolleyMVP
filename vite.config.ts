8c13c902b847e0e8fa6167b391ac2e7503e65ef2
adagioceremonie <adagioceremonie@gmail.com>
2026-09-25 10:02:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig({
+  base: '/adagioceremonie/',
+})
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
